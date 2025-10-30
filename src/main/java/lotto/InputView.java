76d727100d99e2d15179562fb7b1cd07f3379b99
xodbs126 <dltvmsskd@naver.com>
2025-10-31 01:16:03 +0900

    feat: 당첨 번호 및 보너스 번호 입력 로직

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputWinningNumbers() {
+        String winningNumber = Console.readLine();
+        return winningNumber;
+    }
+
+    public String inputBonusNumber() {
+        String bonusNumber = Console.readLine();
+        return bonusNumber;
+    }
+
+
+}
