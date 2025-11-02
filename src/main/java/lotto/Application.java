3050fe6dab89cd2e03c959a5f2d8cb24602c4863
xodbs126 <dltvmsskd@naver.com>
2025-11-03 02:21:03 +0900

    feat : 애플리케이션 의존성 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import lotto.View.InputView;
+import lotto.View.OutputView;
+import lotto.controller.LottoController;
+import lotto.domain.InputParser;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoStore;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.InputValidator;
+import lotto.validator.NumberValidator;
+import lotto.validator.WinningNumbersValidator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputValidator numberValidator = new NumberValidator();
+        InputValidator winningNumbersValidator = new WinningNumbersValidator();
+        InputValidator bonusNumberValidator = new BonusNumberValidator();
+
+        LottoMachine lottoMachine = new LottoMachine();
+
+        InputParser inputParser = new InputParser(numberValidator, winningNumbersValidator, bonusNumberValidator
+        );
+
+        LottoStore lottoStore = new LottoStore(lottoMachine);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        LottoController lottoController = new LottoController(lottoStore, inputParser, inputView, outputView);
+
+        lottoController.play();
     }
 }
