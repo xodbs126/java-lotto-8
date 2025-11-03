b3ab8e7dab18a014f1af2e798e9199e55cc6d1f5
xodbs126 <dltvmsskd@naver.com>
2025-11-03 22:05:24 +0900

    feat : 누락된 출력문 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -5,16 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         String winningNumber = Console.readLine();
         return winningNumber;
     }
 
     public String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
         return bonusNumber;
     }
 
     public String inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
 
         return money;
