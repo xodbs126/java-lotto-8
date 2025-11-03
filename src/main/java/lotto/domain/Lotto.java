d965dd1b965b2531a727bfd5c7e490cfc85d77f5
xodbs126 <dltvmsskd@naver.com>
2025-11-03 19:43:31 +0900

    feat : 로또 숫자 범위 및 중복 유효성 검사 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,8 +13,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> lottoNumbers = new HashSet<>(numbers);
+        if (lottoNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
